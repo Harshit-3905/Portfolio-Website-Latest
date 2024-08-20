8896b9e2075ba281ed3fe4f5c54b96f692bcaf70
Harshit Joshi <harshitjoshileet@gmail.com>
2024-08-20 14:40:45 +0530

    Add projects data to display on the website

---
+++ src/components/miscellaneous/projectsData.ts
@@ -0,0 +1,60 @@
+import BinaryBlogspic from "@/assets/Projects/BinaryBlogs.webp";
+import PortfolioWebsitepic from "@/assets/Projects/PortfolioWebsite.webp";
+import AudioAlleyPic from "@/assets/Projects/AudioAlley.webp";
+import TaskTrackerpic from "@/assets/Projects/TaskTracker.webp";
+import VideoTubePic from "@/assets/Projects/VideoTube.webp";
+
+export const projects = [
+  {
+    name: "Binary Blogs - A Blogging Website",
+    image: BinaryBlogspic,
+    description:
+      "A dynamic platform designed for a seamless and visually engaging blogging experience.",
+    techStack: ["React.js", "TailwindCSS", "Appwrite"],
+    liveLink: "https://binary-blogs.onrender.com/",
+    githubLink: "https://github.com/Harshit-3905/Binary-Blogs",
+  },
+  {
+    name: "Portfolio Website",
+    image: PortfolioWebsitepic,
+    description:
+      "A curated space reflecting my professional persona and passion for web development.",
+    techStack: ["React.js", "ChakraUI"],
+    liveLink: "https://harshitjoshi.netlify.app/",
+    githubLink: "https://github.com/Harshit-3905/Portfolio_Website",
+  },
+  {
+    name: "Audio Alley",
+    image: AudioAlleyPic,
+    description:
+      "An immersive music edtech platform with stunning visuals and intuitive navigation",
+    techStack: ["NextJs", "TailwindCSS", "AceternityUI"],
+    liveLink: "https://audioalley.vercel.app/",
+    githubLink: "https://github.com/Harshit-3905/AudioAlley",
+  },
+  {
+    name: "Task Tracker",
+    image: TaskTrackerpic,
+    description:
+      "A productivity tool designed to streamline tasks and boost focus with advanced features.",
+    techStack: [
+      "React.js",
+      "Redux",
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+      "ChakraUI",
+    ],
+    liveLink: "https://task-tracker-us2h.onrender.com/",
+    githubLink: "https://github.com/Harshit-3905/Task_Tracker",
+  },
+  {
+    name: "VideoTube",
+    image: VideoTubePic,
+    description:
+      "A robust YouTube backend clone with efficient data handling and performance optimization.",
+    techStack: ["Node.js", "Express.js", "MongoDB", "Redis", "Docker"],
+    liveLink: "https://documenter.getpostman.com/view/25112902/2s9Yywdz2v",
+    githubLink: "https://github.com/Harshit-3905/VideoTube-Backend",
+  },
+];
