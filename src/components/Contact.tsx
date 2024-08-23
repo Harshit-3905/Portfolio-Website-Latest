528ef7fe88ca19a72683c08e82f7263cbb1631cd
Harshit Joshi <harshitjoshileet@gmail.com>
2024-08-23 15:55:23 +0530

    Update Contact component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,20 +1,20 @@
 import ContactImage from "../assets/ContactImage.webp";
 import Image from "next/image";
 import ContactForm from "./miscellaneous/ContactFrom";
+import SectionHeading from "./miscellaneous/SectionHeading";
 
 const Contact = () => {
   return (
-    <div className="w-full lg:w-[80%] min-h-[90vh] pt-10" id="contact">
-      <h2 className="pt-10 text-[#F56E0F] text-4xl text-center font-semibold underline underline-offset-5">
-        Get in Contact
-      </h2>
-      <div className="relative flex flex-col md:flex-row w-full min-h-[500px] mt-10 justify-center items-center rounded-2xl py-10 border">
+    <div className="w-full lg:w-[75%] min-h-[90vh]" id="contact">
+      <SectionHeading text="Get in Contact" />
+      <div className="relative flex flex-col md:flex-row w-full min-h-[500px] justify-center items-center rounded-2xl border">
         <div className="flex w-full md:w-1/2 justify-center items-center">
           <Image
             src={ContactImage}
             alt="Contact"
             className="w-80 md:w-[350px] lg:w-[350px] object-cover"
             priority={false}
+            loading="lazy"
             quality={100}
           />
         </div>
