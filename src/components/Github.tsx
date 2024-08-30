3f350d0682d0e4ccf5308a39535b41dad5dac0cd
Harshit Joshi <harshitjoshileet@gmail.com>
2024-08-30 17:28:01 +0530

    Add SectionHeading component and update Github component

--- src/components/Github.tsx
+++ src/components/Github.tsx
@@ -1,13 +1,12 @@
 import GitHubCalendar from "react-github-calendar";
 import { FC } from "react";
+import SectionHeading from "./miscellaneous/SectionHeading";
 
 const Github: FC = () => {
   return (
-    <div id="about" className="w-full lg:w-[75%] pt-10">
-      <h2 className="py-10 text-[#F56E0F] text-3xl font-bold underline underline-offset-5 text-center">
-        My Github Contributions
-      </h2>
-      <div className="flex justify-center">
+    <div id="about" className="w-[80%] lg:w-[75%] pt-10">
+      <SectionHeading text="Github Contributions" />
+      <div className="flex justify-center pt-8">
         <GitHubCalendar username="Harshit-3905" fontSize={20} throwOnError />
       </div>
     </div>
