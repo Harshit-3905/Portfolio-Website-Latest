f72624fe08ac8ccfe2ff461d66767b96d5e509f0
Harshit Joshi <harshitjoshileet@gmail.com>
2024-09-01 07:38:34 +0530

    Add Partcles Background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,18 @@ import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import HireMe from "../components/HireMe";
 import Contact from "../components/Contact";
+import Starfield from "react-starfield";
 import "./globals.css";
 
 function App() {
   return (
     <div className="w-full flex flex-col items-center bg-black text-white overflow-x-hidden scroll-smooth">
+      <Starfield
+        starCount={2000}
+        starColor={[255, 255, 255]}
+        speedFactor={0.07}
+        backgroundColor="black"
+      />
       <Home />
       <Navbar />
       <About />
