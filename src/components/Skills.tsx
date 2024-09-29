dc65ce61a5803d1a964936ff81fe6fc103c64497
Harshit Joshi <harshitjoshileet@gmail.com>
2024-09-29 19:23:45 +0530

    Refactor Skills component to add Nginx skill and icon

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -15,6 +15,7 @@ import {
   PostgresqlIcon,
   AWSIcon,
   PrismaIcon,
+  NginxIcon,
 } from "@/components/miscellaneous/Icons";
 import SectionHeading from "./miscellaneous/SectionHeading";
 
@@ -35,6 +36,7 @@ const skills = [
   { name: "PostgreSQL", icon: PostgresqlIcon },
   { name: "AWS", icon: AWSIcon },
   { name: "Prisma", icon: PrismaIcon },
+  { name: "Nginx", icon: NginxIcon },
 ];
 
 const Skills = () => {
